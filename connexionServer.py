ff916e7c947a3655d61dba2f7730077d8429f315
Katie Graham <graham63@llnl.gov>
2024-11-21 14:44:38 -0800

    Working example with connexion and socketio

--- connexionServer.py
+++ connexionServer.py
@@ -2,9 +2,7 @@ import connexion
 from connexion.resolver import RelativeResolver
 from connexion.middleware import MiddlewarePosition
 from starlette.middleware.cors import CORSMiddleware
-from starlette.middleware.wsgi import WSGIMiddleware
 from flask import jsonify
-from pathlib import Path
 import socketio
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
@@ -21,11 +19,6 @@ connexion_app.add_middleware(
   allow_headers=["*"],
 )
 
-# combined_app = WSGIMiddleware(connexion_app)
-# print(dir(connexion_app.app))
-#print(dir(sio_asgi_app))
-#sio.attach(connexion_app.app)
-
 # API functions
 async def hello():
     return jsonify({"message": "Hello, World!"})
@@ -34,15 +27,15 @@ async def get_site_layout():
   return jsonify({"message": "Site Layout"})
 
 async def app(scope, receive, send):
-    print("Called APP")
     if scope["type"] == "websocket" or scope["path"].startswith("/socket.io"):
         # Handle WebSocket and polling requests with Socket.IO
         await sio_asgi_app(scope, receive, send)
     else:
         # Handle REST API requests with Connexion
-        print("CONNEXION APP")
         with connexion_app.app.app_context():
           await connexion_app(scope, receive, send)
 
-#if __name__ == '__main__':
-  # connexion_app.run(f"{Path(__file__).stem}:app", port=8080, log_level="debug")
+if __name__ == '__main__':
+  import uvicorn
+  uvicorn.run(app, port=8080)
+  
